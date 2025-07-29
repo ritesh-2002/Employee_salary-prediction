3d5e267ec9b42d4ce929958a5e0fb5d18be01531
Ritesh kumar <riteshkumar6661982@gmail.com>
2025-07-30 02:47:08 +0530

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import joblib as joblib
+import joblib
 import numpy as np
 import pandas as pd
 
@@ -7,32 +7,25 @@ st.title("Employee Salary Prediction")
 
 st.divider()
 
+st.write("This is a simple linear regression model to predict employee salaries based on their years of experience and job rate. The model is trained on a dataset containing employee information such as years of experience, job rate, and annual salary. The user can input the years of experience and job rate, and the model will predict the annual salary based on these inputs.")
 
-st.write("this is a simple linear regression model to predict employee salaries based on their years of experience and age. The model is trained on a dataset containing employee information such as years of experience, age, and annual salary. The user can input the years of experience and age, and the model will predict the annual salary based on these inputs.")
+years = st.number_input("Enter the years at company", value=1, step=1, min_value=0)
+jobrate = st.number_input("Enter the job rate", value=3.5, step=0.5, min_value=0.0)
 
-years = st.number_input("Enter the yeas at company", value = 1, step = 1, min_value = 0)
-jobrate = st.number_input("Enter the job rate", value = 3.5, step = 0.5, min_value = 0.0)
-
-x = [years,jobrate]
+x = [years, jobrate]
 
 model = joblib.load("linearmodel.pkl")
 
 st.divider()
 
-predict = st.button("press the button for salary prediction")
+predict = st.button("Press the button for salary prediction")
 
 st.divider()
 
-
 if predict:
     st.balloons()
-    x1 = pd.DataFrame([x], columns=["Years", "Job Rate"])  # Use DataFrame with correct column names
+    x1 = pd.DataFrame([x], columns=["Years", "Job Rate"])  # Use DataFrame with feature names
     prediction = model.predict(x1)
     st.write(f"Salary prediction is ₹{prediction[0]:,.2f}")
-
-
-
-
-
 else:
-  st.warning("please press the button to predict salary")
+   st.warning("Please press the button to predict salary.")
